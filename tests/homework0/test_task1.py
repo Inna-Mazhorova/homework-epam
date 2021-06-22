afc5e21086e39de6b2e7b6ef20ac3eeb0fac9aae
Mazhorova <mazhorova.inna@yandex.ru>
2021-06-23 00:33:08 +0300

    black check

--- tests/homework0/test_task1.py
+++ tests/homework0/test_task1.py
@@ -3,4 +3,3 @@ from homework0.task1 import some_function
 
 def test_some_function():
     assert some_function()
-
--- tests/homework1/test_hw1_task2.py
+++ tests/homework1/test_hw1_task2.py
@@ -1,6 +1,28 @@
 from homework1.task02.fibonacci import check_fibonacci
 
-data_for_check1 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]
+data_for_check1 = [
+    0,
+    1,
+    1,
+    2,
+    3,
+    5,
+    8,
+    13,
+    21,
+    34,
+    55,
+    89,
+    144,
+    233,
+    377,
+    610,
+    987,
+    1597,
+    2584,
+    4181,
+    6765,
+]
 
 
 def test_fibonacci_case():
@@ -8,7 +30,29 @@ def test_fibonacci_case():
     assert check_fibonacci(data_for_check1)
 
 
-data_for_check2 = [0, 1, 1, 2, 3, 5, 8, 13, 22, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]
+data_for_check2 = [
+    0,
+    1,
+    1,
+    2,
+    3,
+    5,
+    8,
+    13,
+    22,
+    34,
+    55,
+    89,
+    144,
+    233,
+    377,
+    610,
+    987,
+    1597,
+    2584,
+    4181,
+    6765,
+]
 
 
 def test_non_fibonacci_case():
