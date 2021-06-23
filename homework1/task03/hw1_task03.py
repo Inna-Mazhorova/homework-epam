6bebf10ed60c926c7c0667aea64db3b3f0228898
Mazhorova <mazhorova.inna@yandex.ru>
2021-06-24 01:34:26 +0300

    task3_correction

--- homework1/task03/hw1_task03.py
+++ homework1/task03/hw1_task03.py
@@ -2,9 +2,7 @@ from typing import Tuple
 
 
 def find_maximum_and_minimum(file_name: str) -> Tuple[int, int]:
-    with open(
-        "C:/Users/Lantana/PycharmProjects/homework-epam/homework1/task03/some_file.txt"
-    ) as fi:
+    with open(file_name) as fi:
         my_lines = fi.read().splitlines()
 
     minimum = int(min(my_lines))
--- tests/homework1/test_hw1_task3.py
+++ tests/homework1/test_hw1_task3.py
@@ -2,9 +2,7 @@ from homework1.task03.hw1_task03 import find_maximum_and_minimum
 
 
 def test_max_min_tuple():
-    assert find_maximum_and_minimum(
-        "C:/Users/Lantana/PycharmProjects/homework-epam/homework1/task03/some_file.txt"
-    ) == (1, 5)
+    assert find_maximum_and_minimum("some_file.txt") == (1, 5)
 
 
 test_max_min_tuple()
