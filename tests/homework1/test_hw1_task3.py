426e202a5d6aa06be221f6dc3b66c9071ef545d3
Mazhorova <mazhorova.inna@yandex.ru>
2021-06-24 15:34:33 +0300

    some_file directory

--- tests/homework1/test_hw1_task3.py
+++ tests/homework1/test_hw1_task3.py
@@ -1,8 +1,10 @@
+import os
+
 from homework1.task03.hw1_task03 import find_maximum_and_minimum
 
 
 def test_max_min_tuple():
-    assert find_maximum_and_minimum("some_file.txt") == (1, 5)
+    assert find_maximum_and_minimum(r"some_file.txt") == (1, 5)
 
 
 test_max_min_tuple()
