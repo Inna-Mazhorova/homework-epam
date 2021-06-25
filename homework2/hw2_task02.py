beb35004130b0c590668441c5e4c77d1aa00f0b0
Mazhorova <mazhorova.inna@yandex.ru>
2021-06-26 02:00:22 +0300

    hw2_task2

---
+++ homework2/hw2_task02.py
@@ -0,0 +1,37 @@
+"""
+Given an array of size n, find the most common and the least common elements.
+The most common element is the element that appears more than n // 2 times.
+The least common element is the element that appears fewer than other.
+You may assume that the array is non-empty and the most common element
+always exist in the array.
+Example 1:
+Input: [3,2,3]
+Output: 3, 2
+Example 2:
+Input: [2,2,1,1,1,2,2]
+Output: 2, 1
+"""
+from typing import List, Tuple, Dict
+
+
+def find_maximum(d: Dict[int, int]) -> int:
+    for elem, freq in d.items():
+        if freq == max(d.values()):
+            return elem
+
+
+def find_minimum(d: Dict[int, int]) -> int:
+    for elem, freq in d.items():
+        if freq == min(d.values()):
+            return elem
+
+
+def major_and_minor_elem(a: List[int]) -> Tuple[int, int]:
+    elements = {}
+    for i in a:
+        if i not in elements:
+            elements[i] = 1
+        else:
+            elements[i] += 1
+    result = (find_maximum(elements), find_minimum(elements))
+    return result
