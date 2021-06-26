4a9218c71d57a4a5bd70cdd16ba640e12894ab5f
Mazhorova <mazhorova.inna@yandex.ru>
2021-06-26 23:51:00 +0300

    hw2_task3

--- homework2/hw2_task03.py
+++ homework2/hw2_task03.py
@@ -1,7 +1,6 @@
 import itertools
 from typing import Any, List
 
-
 # Write a function that takes K lists as arguments and returns all possible
 # lists of K items where the first element is from the first list,
 # the second is from the second and so one.
