657843032c93cac3a50dfa3a9b170f6b52a07e68
Mazhorova <mazhorova.inna@yandex.ru>
2021-06-28 00:01:18 +0300

    hw2_task5

---
+++ homework2/hw2_task05.py
@@ -0,0 +1,63 @@
+import string
+from typing import Any, List
+
+"""
+Some of the functions have a bit cumbersome behavior when we deal with
+positional and keyword arguments.
+Write a function that accept any iterable of unique values and then
+it behaves as range function:
+import string
+assert = custom_range(string.ascii_lowercase, 'g') == ['a', 'b', 'c', 'd', 'e', 'f']
+assert = custom_range(string.ascii_lowercase, 'g', 'p') == ['g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
+assert = custom_range(string.ascii_lowercase, 'p', 'g', -2) == ['p', 'n', 'l', 'j', 'h']
+"""
+dict_ascii = {
+    "a": 0,
+    "b": 1,
+    "c": 2,
+    "d": 3,
+    "e": 4,
+    "f": 5,
+    "g": 6,
+    "h": 7,
+    "i": 8,
+    "j": 9,
+    "k": 10,
+    "l": 11,
+    "m": 12,
+    "n": 13,
+    "o": 14,
+    "p": 15,
+    "q": 16,
+    "r": 17,
+    "s": 18,
+    "t": 19,
+    "u": 20,
+    "v": 21,
+    "w": 22,
+    "x": 23,
+    "y": 24,
+    "z": 25,
+}
+
+
+def custom_range(stop, start="a", step=1):
+    stop_num = dict_ascii[stop]
+    start_num = dict_ascii[start]
+    counter = start_num
+    result = []
+    if step >= 0:
+        while counter < stop_num:
+            result.append(string.ascii_lowercase[counter])
+            counter += step
+        return result
+    else:
+        while counter > stop_num:
+            result.append(string.ascii_lowercase[counter])
+            counter += step
+        return result
+
+
+print(custom_range("g"))
+print(custom_range("p", start="g", step=1))
+print(custom_range("g", start="p", step=-2))
