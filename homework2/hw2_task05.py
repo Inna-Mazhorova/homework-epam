00c3394d8c0572d06efce926ccf16916a27faa06
Mazhorova <mazhorova.inna@yandex.ru>
2021-06-28 14:39:41 +0300

    comments_correction

--- homework2/hw2_task05.py
+++ homework2/hw2_task05.py
@@ -1,16 +1,16 @@
 import string
 from typing import Any, List
 
-"""
-Some of the functions have a bit cumbersome behavior when we deal with
-positional and keyword arguments.
-Write a function that accept any iterable of unique values and then
-it behaves as range function:
-import string
-assert = custom_range(string.ascii_lowercase, 'g') == ['a', 'b', 'c', 'd', 'e', 'f']
-assert = custom_range(string.ascii_lowercase, 'g', 'p') == ['g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
-assert = custom_range(string.ascii_lowercase, 'p', 'g', -2) == ['p', 'n', 'l', 'j', 'h']
-"""
+# Some of the functions have a bit cumbersome behavior when we deal with
+# positional and keyword arguments.
+# Write a function that accept any iterable of unique values and then
+# it behaves as range function:
+# import string
+# assert = custom_range(string.ascii_lowercase, 'g') == ['a', 'b', 'c', 'd', 'e', 'f']
+# assert = custom_range(string.ascii_lowercase, 'g', 'p') == ['g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
+# assert = custom_range(string.ascii_lowercase, 'p', 'g', -2) == ['p', 'n', 'l', 'j', 'h']
+
+
 dict_ascii = {
     "a": 0,
     "b": 1,
