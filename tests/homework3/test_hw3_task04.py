9ee008b348fb54c42abe3f6cc497a713da15c4ab
Mazhorova <mazhorova.inna@yandex.ru>
2021-06-29 10:43:57 +0300

    hw3_task4

--- tests/homework3/test_hw3_task04.py
+++ tests/homework3/test_hw3_task04.py
@@ -2,8 +2,8 @@ from homework3.hw3_task04 import is_armstrong
 
 
 def test_positive_armstrong_case():
-    assert is_armstrong(153) == 'Is Armstrong number'
+    assert is_armstrong(153) == "Is Armstrong number"
 
 
 def test_negative_armstrong_case():
-    assert is_armstrong(10) == 'Is not Armstrong number'
+    assert is_armstrong(10) == "Is not Armstrong number"
