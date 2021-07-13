141dcdedb7837745144bb0835f7357cedb5f6b61
Mazhorova <mazhorova.inna@yandex.ru>
2021-07-13 14:57:08 +0300

    pull_request_correction

--- homework3/hw3_task01.py
+++ homework3/hw3_task01.py
@@ -13,25 +13,21 @@ def cache(times=3):
 
         @functools.wraps(func)
         def count_times(*args, **kwargs):
+            hash = pickle.dumps(args)
+            if hash not in memo:
+                memo[hash] = func(*args)
+                # print("Writing to cache")
+                return memo[hash]
             nonlocal times_called
             times_called += 1
-            hash = pickle.dumps(args)
-
-            if times == 0:
-                result = func(*args)
-                return result
-            elif hash not in memo:
-                print("Writing to cache", times_called)
+            if times_called >= times:
+                del memo[hash]
+                times_called = 0
+                # print("Deliting from cache, Число вызовов", times_called)
                 memo[hash] = func(*args)
                 return memo[hash]
-            elif times_called == (times + 1):
-                print("Deleting from cache", times_called)
-                result = func(*args)
-                del memo[hash]
-                times_called = 1
-                return result
             else:
-                print("Число вызовов", times_called)
+                # print("From cache, Число вызовов", times_called)
                 return memo[hash]
 
         return count_times
