c2e7a87031133c3b26e4396fc7ef24ccea4b4bf1
Mazhorova <mazhorova.inna@yandex.ru>
2021-08-17 14:01:34 +0300

    hw12_improvements

--- homework12/school/teacher/admin.py
+++ homework12/school/teacher/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Person, Teacher, Student, Homework, HomeworkResult
 
 # Register your models here.
+
+admin.site.register(Person)
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Homework)
+admin.site.register(HomeworkResult)
--- homework12/school/teacher/models.py
+++ homework12/school/teacher/models.py
@@ -1,32 +1,64 @@
+import datetime
+from collections import defaultdict
+from typing import Union
+
 from django.db import models
 
 
 # Create your models here.
-class Person(models.Model):
-    last_name = models.CharField(max_length=50)
-    first_name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return f"{self.last_name} {self.first_name}"
-
-
-class Teacher(Person, models.Model):
-    def __str__(self):
-        return f"{self.last_name} {self.first_name}"
-
-
-class Student(Person, models.Model):
-    def __str__(self):
-        return f"{self.last_name} {self.first_name}"
-
-
 class Homework(models.Model):
     text = models.TextField()
     deadline = models.DurationField()
     created = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.text
+
+    def is_active(self) -> bool:
+        date_due = self.created + self.deadline
+
+        return datetime.datetime.now() <= date_due
+
+
+class Person(models.Model):
+    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.last_name} {self.first_name}"
+
+
+class Teacher(Person, models.Model):
+    def __str__(self) -> str:
+        return f"{self.last_name} {self.first_name}"
+
+    homework_done = defaultdict(set)
+
+    def create_homework(self, text: str, deadline: int):
+        return Homework(text, deadline)
+
+    def check_homework(self, homework_result) -> bool:
+        if len(homework_result.solution) < 5:
+            return False
+
+        self.homework_done[homework_result.homework] = homework_result.solution
+        return True
+
+    @classmethod
+    def reset_results(cls, homework_unit: Homework = None) -> None:
+
+        if homework_unit:
+            del cls.homework_done[homework_unit]
+        else:
+            cls.homework_done.clear()
+
+
+class Student(Person, models.Model):
+    def __str__(self) -> str:
+        return f"{self.last_name} {self.first_name}"
+
+    def do_homework(self, homework: str, solution: str) -> Union[None, "Homework"]:
+        return HomeworkResult(self, homework, solution)
 
 
 class HomeworkResult(models.Model):
@@ -35,5 +67,5 @@ class HomeworkResult(models.Model):
     solution = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.homework
